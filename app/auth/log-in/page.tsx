19024c6fb3d680a2e737c29e4a9f9243b3cbed5e
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-25 18:17:32 +0300

    Feat log-in page

--- app/auth/log-in/page.tsx
+++ app/auth/log-in/page.tsx
@@ -1,6 +1,41 @@
+import Button from '@/app/ui/button/button';
+import style from './page.module.scss'
+import Input from '@/app/ui/input/input';
+import Image from 'next/image';
+import LogInImage from '@/public/log-in-img/log_in-image.png'
 
-export default function Page() {
+const Page:React.FC = () => {
   return (
-    <h1> Log-in </h1>
+    <section className={style.log_in__section}>
+    <div >
+    <h1 className={style.log_in__title}> Log in </h1>
+    <form className={style.log_in__form}>
+    <Input
+    placeholder='Email' 
+    inputClass={style.log_in__input}
+    />
+    <p className={style.log_in__description}>
+    Enter your email
+    </p>
+    <Input
+    placeholder='Password'
+    inputClass={style.log_in__input}
+    />
+    <p className={style.log_in__description}>
+    Enter your password
+    </p>
+    </form>
+    <Button 
+    text='Log In'
+    className={style.log_in__button}
+    /> 
+    </div>  
+    <Image 
+    src = {LogInImage}
+    alt = 'Men read'
+    />
+    </section>
   );
 }
+
+export default Page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
-
+import style from './page.module.scss'
 import { poppins } from './ui/fonts';
+import Footer from './ui/footer/footer';
+import Header from './ui/header/header';
 
-const RootLayout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
+const RootLayout = ({ children }: Readonly<{children: React.ReactNode; }>) => {
+
+
   return (
     <html lang="en" className={poppins.className}>
       <body>
+      <div className={style.wrapper__inner}>
+        <Header />
         {children}
+      </div> 
+        <Footer />
       </body>
     </html>
   );

9bf4324c96d82248528941ebe8ea69987b1e4870
Alex Koloda <xeniumsonyz@gmail.com>
2025-01-09 17:58:24 +0300

    Example

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,41 @@
+import { Metadata } from 'next';
+import Hero from './ui/hero/hero';
+import Catalog from './ui/catalog/catalog';
+import Banner from './ui/banner/banner';
+import style from './page.module.scss'
+// import { cookies } from 'next/headers';
+// import { redirect } from 'next/dist/server/api-utils';
 
-export default function Home() {
-  return (
-    <h1> Book Store </h1>
-  );
+
+export const metadata: Metadata = {
+  title: 'Home page', 
 }
+
+// const protectPage = async (path: string) => {
+//   try {
+//     const cookiesValues = await cookies();
+//     const response = await fetch('/me', {
+//       method: 'GET',
+//       headers: {
+        
+//       }
+//     });
+//     const data = await response.json()
+//     return data;
+//   } catch(err) {
+//     redirect(path)
+//   }
+// }
+
+const Page = async () => {
+  // await protectPage('/auth');
+  return (
+    <main className={style.main}>
+    <Hero />
+    <Catalog />
+    <Banner />
+    </main>
+  )
+}
+
+export default Page;

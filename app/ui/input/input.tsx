499cdf3b3ffe478f55ba1b3f02ad2979fd04768d
Alex Koloda <xeniumsonyz@gmail.com>
2025-01-09 17:55:35 +0300

    Add name, type, onChange in TInput

--- app/ui/input/input.tsx
+++ app/ui/input/input.tsx
@@ -1,12 +1,18 @@
 type TInput = {
+  name: string;
+  type: string;
   placeholder: string;
   inputClass: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 
 const Input: React.FC <TInput> = (props) => {
   return (
     <input 
+    name={props.name}
+    onChange={props.onChange}
+    type={props.type}
     placeholder={props.placeholder}
     className={props.inputClass}
     ></input>

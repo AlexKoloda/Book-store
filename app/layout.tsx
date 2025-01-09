c09697114fc00534ce251506d0215f728ec6e86b
Alex Koloda <xeniumsonyz@gmail.com>
2025-01-09 17:53:16 +0300

    Feat clsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
+import clsx from 'clsx';
 import style from './page.module.scss'
 import { poppins } from './ui/fonts';
 import Footer from './ui/footer/footer';
 import Header from './ui/header/header';
 
+
 const RootLayout = ({ children }: Readonly<{children: React.ReactNode; }>) => {
 
 
   return (
-    <html lang="en" className={poppins.className}>
-      <body>
+    <html lang="en" className={clsx(poppins.className, style.body)}>
+      <body className={style.body}>
       <div className={style.wrapper__inner}>
         <Header />
         {children}

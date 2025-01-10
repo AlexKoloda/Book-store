20a4f090cfffd0d0ca6b0cb5e322057d334106fa
Alex Koloda <xeniumsonyz@gmail.com>
2025-01-10 17:56:51 +0300

    Refactor

--- app/auth/log-in/page.tsx
+++ app/auth/log-in/page.tsx
@@ -6,74 +6,77 @@ import Image from 'next/image';
 import LogInImage from '@/public/log-in-img/log_in-image.png';
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { redirect } from 'next/navigation';
+import conf from '@/config';
 
+const url = conf.url;
 
 const LoginPage: React.FC = () => {
-  
   const [data, setData] = useState({
     email: '',
     password: '',
   });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => { 
-
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setData((prev) => ({
       ...prev,
       [e.target.name]: e.target.value,
     }));
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement> ) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const res = await fetch(`http://localhost:4000/auth/sign-in`, {
+      const res = await fetch(`${url}/auth/sign-in`, {
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         method: 'POST',
         body: JSON.stringify({
           email: data.email,
           password: data.password,
-      })
-    })
-        const user = await res.json()
-        console.log(user)
+        }),
+      });
+      const user = await res.json();
+      debugger;
+      console.log(user);
+      return user;
     } catch (error) {
-      console.log( error )
+      console.log(error);
+      redirect('/auth/log-in');
     }
-  }
-
+  };
 
   return (
     <section className={style.log_in__section}>
       <div>
         <h1 className={style.log_in__title}> Log in </h1>
-        <form
-        onSubmit={handleSubmit}
-        className={style.log_in__form}>
+        <form 
+          action='/'
+          onSubmit={handleSubmit}
+          className={style.log_in__form}>
           <Input
-            name = 'email'
+            name='email'
             onChange={handleChange}
             type='email'
             placeholder='Email'
             inputClass={style.log_in__input}
-          />
+            />
           <p className={style.log_in__description}>Enter your email</p>
           <Input
-            name = 'password'
+            name='password'
             onChange={handleChange}
             type='password'
             placeholder='Password'
             inputClass={style.log_in__input}
-          />
+            />
           <p className={style.log_in__description}>Enter your password</p>
+        </form>
           <Button
             type='submit'
             text='Log In'
             className={style.log_in__button}
-          />
-        </form>
-      </div>
+            />
+            </div>
       <Image src={LogInImage} alt='Man read' />
     </section>
   );

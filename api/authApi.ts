1b9bb9cc241449c10c6ed6fe09013e66e3181a53
Alex Koloda <xeniumsonyz@gmail.com>
2025-01-15 17:35:02 +0300

    Rename

--- api/authApi.ts
+++
@@ -1,7 +0,0 @@
-import { TUser, TUserSignIn } from '@/app/lib/definitions';
-import axiosApi from './axios'
-
-
-export const signInApi = (body: TUserSignIn) => {
-  return axiosApi.post<{user: TUser, token: string}>(`/auth/sign-in`, body);
-  };

6966b434f73c6c5ef0442574c8cf785ebeaae447
Alex Koloda <xeniumsonyz@gmail.com>
2025-01-15 17:33:27 +0300

    Fix types

--- public/validation/schemas.ts
+++ public/validation/schemas.ts
@@ -2,12 +2,23 @@ import { string, object } from 'yup';
 
 export const logInSchema = object().shape({
   email: string().email('Email is not correct').required('Email required'),
-  password: string().min(6, 'Password must be longer than 6').required('Password required'),
+  password: string()
+    .min(6, 'Password must be longer than 6')
+    .required('Password required'),
 });
 
 export const signUpSchema = object().shape({
   email: string().email('Email is not correct').required('Email required'),
-  password: string().min(6, 'Password must be longer than 6').required('Password required'),
-  passwordReplay: string().min(6, 'Password must be longer than 6').required('Password required').oneOf([('password'), 'Passwords are different']),
+  password: string()
+    .min(6, 'Password must be longer than 6')
+    .required('Password required'),
+  passwordReplay: string()
+    .min(6, 'Password must be longer than 6')
+    .required('Password required')
+    .oneOf(['password', 'Passwords are different']),
 });
 
+export const userSchema = object().shape({
+  name: string().min(1, 'Name must be longer than 1').required(),
+  email: string().email('Email is not correct').required('Email required'),
+});

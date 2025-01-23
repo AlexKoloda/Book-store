c5e7ee1f7c3b823e8f597118f7f42681fbd17df9
Alex Koloda <xeniumsonyz@gmail.com>
2025-01-23 18:01:20 +0300

    Update api for pagination

--- api/serverApi/getBook.ts
+++ api/serverApi/getBook.ts
@@ -1,19 +1,11 @@
 'use server';
-
 import { IBook } from '@/app/lib/definitions';
 import conf from '@/config';
 
-export const getBooks = async () => {
-
-  try {
-    const res = await fetch(`${conf.url}/book/`, {
+export const getBooksApi = async (data: string) => {
+    const res = await fetch(`${conf.url}/book?page=${data}`, {
       method: 'GET',
-    });
+    })
     const books = await res.json();
-    return books as IBook[];
-    
-  } catch (error) {
-    //TODO Реализовать кастомные ошибки
-    console.log(error)
-  }
+    return books as IBook[];    
 }

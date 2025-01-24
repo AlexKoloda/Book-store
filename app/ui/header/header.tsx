5f7975ae08a7b3808bbdee0a494ac868df5b8d77
Alex Koloda <xeniumsonyz@gmail.com>
2025-01-24 17:08:46 +0300

    Feat toggle sign-up/log-in

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,18 +1,31 @@
-'use client'
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
 import style from './Header.module.scss';
 import Logo from '@/public/header-img/header-logo.png';
-import SearchLogo from '@/public/icons/Search.png'
+import SearchLogo from '@/public/icons/Search.png';
 import Button from '../Button/Button';
 import Form from 'next/form';
 import { useUserContext } from '@/app/lib/contexts/UserContext';
 import Menu from '../Menu/Menu';
 import Input from '../Input/Input';
-
+import { redirect, usePathname } from 'next/navigation';
 
 const Header: React.FC = () => {
   const user = useUserContext();
+  const logInPath = usePathname() === '/auth/log-in';
+  const signUpPath = usePathname() === '/auth/sign-up';
+
+  const handleCLick = () => {
+    if (signUpPath) {
+      redirect('/auth/log-in');
+    }
+    if (logInPath) {
+      redirect('/auth/sign-up');
+    }
+
+    redirect('/auth/log-in');
+  };
 
   return (
     <header className={style.header}>
@@ -28,24 +41,30 @@ const Header: React.FC = () => {
       <p className={style.header__catalog}>Catalog</p>
 
       <label className={style.header__label}>
-      <div className={style.header__search}>
-        <Image src={SearchLogo} alt='Search logo' className={style.header__search_logo}/> 
-      <Form action='search'>
-        <Input
-          type='search'
-          name='search'
-          placeholder='Search'
-          className={style.header__input}
+        <div className={style.header__search}>
+          <Image
+            src={SearchLogo}
+            alt='Search logo'
+            className={style.header__search_logo}
           />
-      </Form>
-      </div>
+          <Form action='search'>
+            <Input
+              type='search'
+              name='search'
+              placeholder='Search'
+              className={style.header__input}
+            />
+          </Form>
+        </div>
       </label>
-       { user.user ? (
-        < Menu />
+      {user.user ? (
+        <Menu />
       ) : (
-        <Link href='/auth/log-in'>
-        <Button text='Log In/ Sing Up' className={style.header__button} />
-      </Link>
+        <Button
+          text='Log In/ Sing Up'
+          className={style.header__button}
+          onClick={handleCLick}
+        />
       )}
     </header>
   );

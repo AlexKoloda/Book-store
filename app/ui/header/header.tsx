253b05b1935430689810d44b1413c270f663dac8
Alex Koloda <xeniumsonyz@gmail.com>
2025-02-24 11:55:12 +0300

    Fix

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,48 +1,50 @@
-'use client';
-import style from './Header.module.scss';
-import { useState } from 'react';
-import { useUserContext } from '@/app/lib/contexts/UserContext';
-import { redirect, usePathname, useRouter } from 'next/navigation';
-import queryString from 'query-string';
+"use client";
+import style from "./Header.module.scss";
+import { useState } from "react";
+import { useUserContext } from "@/app/lib/contexts/UserContext";
+import { redirect, usePathname, useRouter } from "next/navigation";
+import queryString from "query-string";
 
-import Form from 'next/form';
-import Link from 'next/link';
-import Image from 'next/image';
-import Menu from '../Menu/Menu';
-import Input from '../Input/TextInput';
-import Button from '../Button/Button';
-import SearchLogo from '@/public/icons/Search.png';
-import Logo from '@/public/header-img/header-logo.png';
+import Form from "next/form";
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "../Menu/Menu";
+import Input from "../Input/TextInput";
+import Button from "../Button/Button";
+import SearchLogo from "@/public/icons/Search.png";
+import Logo from "@/public/header-img/header-logo.png";
 
 const Header: React.FC = () => {
-  const {user} = useUserContext();
-  const {replace} = useRouter();
-  const logInPath = usePathname() === '/auth/log-in';
-  const signUpPath = usePathname() === '/auth/sign-up';
-  const [queryParams, setQueryParams] = useState('');
+  const { user } = useUserContext();
+  const { replace } = useRouter();
+  const logInPath = usePathname() === "/auth/log-in";
+  const signUpPath = usePathname() === "/auth/sign-up";
+  const [queryParams, setQueryParams] = useState("");
 
   const handleToggleLogInButton = () => {
     if (signUpPath) {
-      redirect('/auth/log-in');
+      redirect("/auth/log-in");
     }
     if (logInPath) {
-      redirect('/auth/sign-up');
+      redirect("/auth/sign-up");
     }
-    redirect('/auth/log-in');
+    redirect("/auth/log-in");
   };
 
   const handleSearch = (value: string) => {
-   setQueryParams(queryString.stringify(
-      {
-        value
-      },
-      { arrayFormat: 'comma', skipNull: true }
-    ))  
+    setQueryParams(
+      queryString.stringify(
+        {
+          value,
+        },
+        { arrayFormat: "comma", skipNull: true }
+      )
+    );
   };
 
   const handleSubmitSearch = () => {
-    replace(`/${queryParams}`)
-  }
+    replace(`/${queryParams}`);
+  };
 
   return (
     <header className={style.header}>
@@ -77,8 +79,7 @@ const Header: React.FC = () => {
         </div>
       </label>
       {user ? (
-        <Menu 
-        />
+        <Menu />
       ) : (
         <Button
           text='Log In/ Sing Up'
